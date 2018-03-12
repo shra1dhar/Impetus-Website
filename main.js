c280a3bdd1c622b908b6c01359a50caef2b58a5a
Shravan Dhar <shraonedhar@gmail.com>
2018-03-12 20:30:38 +0530

    gallery page done amd responsiveness added to it

--- main.js
+++ main.js
@@ -15,25 +15,43 @@ $(document).ready(function () {
     });
 });
 
- // Set the date we're counting down to
-var countDownDate = new Date("Mar 23, 2018 00:00:00").getTime();
-// Update the count down every 1 second
-var x = setInterval(function () {
-    // Get todays date and time
-    var now = new Date().getTime();
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Display the result in the element with id="demo"
-    document.getElementById("time").innerHTML = days + "d " + hours + "h " +
-        minutes + "m " + seconds + "s ";
-    // If the count down is finished, write some text 
+
+
+const countdown = document.querySelector('.countdown');
+
+// Set Launch Date (ms)
+const launchDate = new Date('Mar 23, 2018 09:00:00').getTime();
+
+// Update every second
+const intvl = setInterval(() => {
+    // Get todays date and time (ms)
+    const now = new Date().getTime();
+
+    // Distance from now and the launch date (ms)
+    const distance = launchDate - now;
+
+    // Time calculation
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display result
+    countdown.innerHTML = `
+  <div>${days}<span>&nbsp&nbspDays&nbsp&nbsp</span></div> 
+  <div>${hours}<span>&nbspHours&nbsp</span></div>
+  <div>${mins}<span>Minutes</span></div>
+  <div>${seconds}<span>Seconds</span></div>
+  `;
+
+    // If launch date is reached
     if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "Started";
+        // Stop countdown
+        clearInterval(intvl);
+        // Style and output text
+        countdown.style.color = '#17a2b8';
+        countdown.innerHTML = 'Launched!';
     }
 }, 1000);
